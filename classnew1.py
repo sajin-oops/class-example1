8c4c73294898bd593f0a9e5de482cf2d8823eabd
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-29 19:50:33 +0530

    Add files via upload

---
+++ classnew1.py
@@ -0,0 +1,26 @@
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name 
+#     self.age = age
+
+#   def __str__(self):
+#     return f"{self.name}({self.age})"    
+
+# p1 = Person("Sajin", 21)
+
+# print(p1)
+
+# O/P - Sajin(21)
+
+class ak:
+  def __init__(self,name):
+    self.name = name
+
+    
+  def __str__(self):
+    return f"{self.name}"
+  
+obj1 = ak("Nijas")
+print(obj1)
+
+#O/P - Nijas
